d5909303f7232c52d14644758dfb451ca71fdce6
0xFFCat <70032504+ZueffC@users.noreply.github.com>
2022-11-18 18:06:57 +0300

    added first command: search

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module mirage-cli
 go 1.19
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/go-resty/resty/v2 v2.7.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/urfave/cli/v2 v2.16.3 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
 )
--- internal/initialize.go
+++ internal/initialize.go
@@ -1,22 +1,44 @@
 package internal
 
 import (
-	"mirage-cli/internal/commands"
+	"fmt"
+	"mirage-cli/internal/additions"
+	"os"
+	"reflect"
 	"strconv"
 	"time"
 
+	"github.com/BurntSushi/toml"
 	"github.com/urfave/cli/v2"
 )
 
 var App cli.App
 
+type Nodes struct {
+	Nodes []string
+}
+
 func Initialize() {
+	var config Nodes
+
 	yearNow := strconv.Itoa(time.Now().Year())
+	_, err := toml.DecodeFile("nodes.toml", &config)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	typ, val := reflect.TypeOf(config), reflect.ValueOf(config)
+	nodes := make([]string, typ.NumField())
+
+	for i := 0; i < typ.NumField(); i++ {
+		nodes[i] = fmt.Sprintf("%v", val.Field(i).Interface())
+	}
 
 	App.Name = "mirage"
 	App.Usage = "blazingly fast package manager"
 	App.UsageText = "mirage [command] [name of package] [flags]"
-	App.Copyright = "(C) " + yearNow + " 0xFFCat. By WTFPL License"
+	App.Copyright = "(C) " + yearNow + " ZueffC. By WTFPL License"
 
 	App.Authors = []*cli.Author{
 		{
@@ -26,7 +48,18 @@ func Initialize() {
 	}
 
 	App.Commands = []*cli.Command{
-		commands.Search(), //Search command
+		&cli.Command{
+			Name:        "search",
+			Aliases:     []string{"sch", "s"},
+			Description: "this command will search package by name on nodes from yor .config file",
+			Action: func(ctx *cli.Context) error {
+				name := ctx.Args().Get(0)
+				res := additions.SearchByNameQuery(name, nodes[0][1:len(nodes[0])-1])
+				println(res.GitUrl)
+
+				return nil
+			},
+		},
 	}
 
 }
