69a88bbb2a68e5b6142e5571dd30c30de0050146
br-netw <40954245+br-netw@users.noreply.github.com>
2022-11-30 15:11:14 +0300

    Update printInfo.go

--- internal/additions/printInfo.go
+++ internal/additions/printInfo.go
@@ -4,6 +4,6 @@ import "github.com/fatih/color"
 
 func PrintInfo(res *PackageData) {
 	color.Cyan("[INFO] Found 1 package: %s", res.Name)
-	color.Cyan("[INFO] It`s description:")
+	color.Cyan("[INFO] Its description:")
 	color.Yellow(res.Description)
 }
