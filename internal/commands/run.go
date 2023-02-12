b20d5d4d63ba2284bce241b65c5ec4c21cc6362e
0xFFCat <70032504+ZueffC@users.noreply.github.com>
2023-02-12 22:28:33 +0300

    Update run.go

--- internal/commands/run.go
+++ internal/commands/run.go
@@ -1,16 +1,37 @@
-package commands
+package actions
 
 import (
-	"mirage-cli/internal/commands/actions"
+	"mirage-cli/internal/additions"
+	"mirage-cli/internal/parsers"
+	log "mirage-cli/packages/logger"
+	"os"
+	"os/exec"
 
 	"github.com/urfave/cli/v2"
 )
 
-func Run() *cli.Command {
-	return &cli.Command{
-		Name:        "run",
-		Aliases:     []string{"run", "-r", "r"},
-		Description: "This command will run package you want",
-		Action:      actions.RunAction,
+func RunAction(ctx *cli.Context) error {
+	name := ctx.Args().Get(0)
+
+	if flag, dirName := additions.IsInstalled(name); flag == true {
+		path := parsers.ParseHomePath() + "/.mirage/" + dirName
+		pkgInfo := parsers.ParsePackageInfo(path)
+
+		for _, v := range pkgInfo.RunApplication {
+			cmd := exec.Command("/bin/sh", "-c", v)
+			cmd.Stderr, cmd.Stdin, cmd.Stdout = os.Stderr, os.Stdin, os.Stdout
+			cmd.Dir = path
+			err := cmd.Run()
+
+			if err != nil {
+				(&log.Message{
+					Type:    log.Error,
+					Message: "There is an error in run script.",
+				}).Log()
+			}
+		}
+
 	}
+
+	return nil
 }
